5c73597dfd797ba5e77c0aee28330111c27351a3
Simon Thompson <simon@chi.swan.ac.uk>
2026-03-19 21:29:03 +0000

    Refactor Docker installation in install-docker.sh
    Updated Docker installation script for Debian. Added keyring management and specified Docker version.

--- install-docker.sh
+++ install-docker.sh
@@ -6,10 +6,17 @@ apt install apt-transport-https ca-certificates git curl software-properties-com
 
 curl -fsSL https://download.docker.com/linux/debian/gpg |  gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" |  tee /etc/apt/sources.list.d/docker.list
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-apt update
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian trixie stable" |   sudo tee /etc/apt/sources.list.d/docker.list
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+DOCKER_VERSION="5:29.1.4-1~debian.13~trixie"
+sudo apt install docker-ce=$DOCKER_VERSION docker-ce-cli=$DOCKER_VERSION containerd.io docker-buildx-plugin docker-compose-plugin
 
 systemctl enable docker
 systemctl start docker
